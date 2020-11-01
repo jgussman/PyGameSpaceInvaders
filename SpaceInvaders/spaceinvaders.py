6b724320158233af203e8e6007c1cd40294cda9e
Nicholas Mobley <nmobley45@gmail.com>
2020-11-01 18:10:57 -0500

    game finished sans aestetics

--- SpaceInvaders/spaceinvaders.py
+++ SpaceInvaders/spaceinvaders.py
@@ -97,7 +97,7 @@ class Alien():
                                      [x,y,h,w])
         self.direction = 0
     
-    def draw(self,surface):
+    def draw(self):
         pygame.draw.rect(self.gameDisplay,Alien.white,
                          [self.x,self.y,self.h,self.w])
         for bullet in Alien.bullets: bullet.draw()
@@ -121,6 +121,7 @@ class Alien():
         y = self.y + self.h
         Alien.bullets.append(Alien_Bullet(x,y,self.gameDisplay)) 
 
+
 class None_Alien(Alien):
 
     def __init__(self):
@@ -128,7 +129,8 @@ class None_Alien(Alien):
         self.y = 0
         self.h = 0
         self.w = 0
-    def draw(self,surface):
+
+    def draw(self):
         pass
 
     def move_side(self):
@@ -142,6 +144,7 @@ class None_Alien(Alien):
 
     def fire_bullet(self):
         pass
+
 
 class Defender():
 
@@ -185,7 +188,6 @@ class Game:
     display_height = 600
     black = (0,0,0)
     white = (255,255,255)
-
     columns = range(88,413,27)
     rows = range(20,126,21)
     nAliens = 72
@@ -197,7 +199,6 @@ class Game:
     player_move = pygame.USEREVENT + 6
     NONE_ALIEN = None_Alien()
     NONE_DEFENDER_BULLET = None_Defender_Bullet()
-
     clock = pygame.time.Clock()
 
     def listReplace(lst,old,new):
@@ -210,14 +211,14 @@ class Game:
                 lst[index] = new
                 break
 
-    def __init__(self,player):
+    def __init__(self,player,level = 1, score = 0,lives = 3):
         """
         Takes a player OBJECT as the argument. 
         """
         pygame.init()
         pygame.display.set_caption('Space Invaders')
         self.gameDisplay = pygame.display.set_mode((Game.display_width,Game.display_height))
-        self.gameScore = 0
+        self.gameScore = score
         self.gameExit = False
         self.armada = []
         for c in Game.columns:
@@ -225,6 +226,34 @@ class Game:
                     self.armada.append(Alien(c,r,'red',self.gameDisplay))
         self.player = player
         self.defender = Defender(self.gameDisplay,Game.display_width,Game.display_height)
+        self.lives = lives
+        self.level = level
+        self.kills = 0
+
+    def soft_reset(self):
+        self.player.x = Game.display_width // 2
+        Alien.bullets = []
+        self.defender.bullets = []
+        self.gameDisplay.fill(Game.black)
+        self.defender.draw()
+        for alien in self.armada: alien.draw()
+
+    def hard_reset(self):
+        self.armada = []
+        for c in Game.columns:
+            for r in Game.rows:
+                self.armada.append(Alien(c,r,'red',self.gameDisplay))
+        self.kills = 0
+        self.defender.bullets = []
+        self.level += 1
+        Alien.bullets = []
+        Alien.xvelocity = 4 + self.level
+        Alien.yvelocity = 9 + (self.level*2)
+        self.player.x = Game.display_width // 2
+        self.gameDisplay.fill(Game.black)
+        self.defender.draw()
+        for alien in self.armada: alien.draw()
+
 
 
     def playGame(self):
@@ -284,7 +313,7 @@ class Game:
             self.defender.draw()
             for bullet in self.defender.bullets: bullet.tick()
             for bullet in Alien.bullets: bullet.tick()
-            for alien in self.armada: alien.draw(self.gameDisplay)
+            for alien in self.armada: alien.draw()
             # Collisions
             for bullet in self.defender.bullets:
                 for alien in self.armada:
@@ -294,14 +323,26 @@ class Game:
                         Game.listReplace(self.armada,
                                     alien,
                                     Game.NONE_ALIEN)
-                        self.gameScore += 3
+                        self.gameScore += 3 * (self.level)
+                        self.kills += 1
             for bullet in Alien.bullets:
                 if bullet.y > Game.display_height:
                     Alien.bullets.remove(bullet)
-                    self.gameScore += 1
+                    self.gameScore += 1 * (self.level)
                 if (bullet.x in range(int(self.defender.x),int(self.defender.x+self.defender.w)+1) and
                     bullet.y in range(int(self.defender.y),int(self.defender.y+self.defender.h)+1)):
+                    self.lives -= 1
+                    if self.lives == 0:
+                        self.gameExit = True
+                    self.soft_reset()
+                    pygame.time.wait(1000)
+                    break
+            for alien in self.armada:
+                if alien.y >= self.defender.y:
                     self.gameExit = True
+            if self.kills == Game.nAliens:
+                self.hard_reset()
+                pygame.time.wait(1000)
             pygame.display.update()
 
             Game.clock.tick(60)
