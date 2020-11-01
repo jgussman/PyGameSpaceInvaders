00945634b690caef42614c668409ec7c6bb06a5e
Brady <brjoande@iu.edu>
2020-11-01 05:19:45 -0500

    some design writing and understanding code

--- SpaceInvaders/spaceinvaders.py
+++ SpaceInvaders/spaceinvaders.py
@@ -35,7 +35,18 @@ class Alien():
     def draw(self,surface):
         pygame.draw.rect(gameDisplay,white,
                          [self.x,self.y,self.h,self.w])
+    
+    def update(self):
+        '''
+        to update the alien....going to need to have awareness of rows
+        rows will also move down and repeatedly go left/right
 
+        given the whole sprite list will find a range to conditonally check when to go up/down 
+        will be based on a all_sprites_list
+
+        will also be based on updating properties of self
+        '''
+        pass
 
 class Bullet():
 
@@ -51,7 +62,6 @@ class Bullet():
     def update(self):
         self.y -= 5
         self.rect.move_ip(self.x,self.y)
-        
 
 class Defender():
 
@@ -69,6 +79,9 @@ class Defender():
         self.bullet_y = y
         self.bullet_h = bullet_h
         self.bullet_w = bullet_w
+
+        # dealings w/ ghosts
+        self.width = 0
     
     def update(self):
         keys = pygame.key.get_pressed()
@@ -83,6 +96,7 @@ class Defender():
                 self.x = display_width- self.w
             self.rect.move_ip(self.x,self.y)
         if keys[pygame.K_SPACE]:
+            ## add processing here to only trigger fire_bullet when there is non existant bullet here
             self.fire_bullet()
         if not (self.bullet is None):
             self.update_bullet()
@@ -117,6 +131,15 @@ class Defender():
         return self.x
 
 def game_loop():
+    '''
+    Main Loop of games
+    Include:
+    1.events
+    2.collisions
+    3.bullet system tracker
+    4.clock
+    5.sprites
+    '''
     gameExit = False
     bullet = None
     while not gameExit:
@@ -129,7 +152,7 @@ def game_loop():
         gameDisplay.fill(black) 
         defender.draw(gameDisplay)
         alien.draw(gameDisplay)  
-        print(defender.x)    
+        print(defender.x)
         defender.update()
         pygame.display.update()
 
