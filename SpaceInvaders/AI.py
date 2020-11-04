50d68023b663366e32d9790769915834f914f22c
userjude <userjude@parchaastroindianaedu.local>
2020-11-03 22:27:14 -0500

    Big Problem... Probably have to really rethinkthis

--- SpaceInvaders/AI.py
+++ SpaceInvaders/AI.py
@@ -2,8 +2,10 @@ from Knuckle import Player
 from skimage import transform 
 from skimage.color import rgb2gray 
 from collections import deque 
-
-
+import tensorflow as tf 
+import QLearning
+import Memory
+import numpy as np 
 
 
 class QLearningPlayer(Player):
@@ -28,7 +30,7 @@ class QLearningPlayer(Player):
             3.3: 
 
         """
-        print("INPUT VECTORR HEERE",inputVector)
+        print("INPUT VECTORR HEERE",inputVector.shape)
         possible_actions = [[1,0,0],[0,1,0],[0,0,1]] #One-Hot encoded 
         # Dimensions 500, 668, 3 
 
@@ -68,8 +70,182 @@ class QLearningPlayer(Player):
         ## TURN THIS TO TRUE IF YOU WANT TO RENDER THE ENVIRONMENT
         episode_render = False
 
+        # Reset The Graph 
+        tf.reset_default_graph()
 
-        self.preprocess(inputVector)
+        # Instantiate the Deep Learning 
+        network = QLearningNet(state_size,action_size,learning_rate)
+
+        # Setup TensorBoard Writer
+        writer = tf.summary.FileWriter("/tensorboard/dqn/1")
+
+        ## Losses
+        tf.summary.scalar("Loss", DQNetwork.loss)
+
+        write_op = tf.summary.merge_all()
+
+
+        # Instantiate memory
+        memory = Memory(max_size = memory_size)
+        for i in range(pretrain_length):
+            # If it's the first step
+            if i == 0:
+                state = env.reset()
+                
+                state, stacked_frames = stack_frames(stacked_frames, state, True)
+                
+            # Get the next_state, the rewards, done by taking a random action
+            choice = random.randint(1,len(possible_actions))-1
+            action = possible_actions[choice]
+            next_state, reward, done, _ = env.step(action)
+            
+            #env.render()
+            
+            # Stack the frames
+            next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+            
+            
+            # If the episode is finished (we're dead 3x)
+            if done:
+                # We finished the episode
+                next_state = np.zeros(state.shape)
+                
+                # Add experience to memory
+                memory.add((state, action, reward, next_state, done))
+                
+                # Start a new episode
+                state = env.reset()
+                
+                # Stack the frames
+                state, stacked_frames = stack_frames(stacked_frames, state, True)
+                
+            else:
+                # Add experience to memory
+                memory.add((state, action, reward, next_state, done))
+                
+                # Our new state is now the next_state
+                state = next_state
+        # Saver will help us to save our model
+        saver = tf.train.Saver()
+
+        if training == True:
+            with tf.Session() as sess:
+                # Initialize the variables
+                sess.run(tf.global_variables_initializer())
+
+                # Initialize the decay rate (that will use to reduce epsilon) 
+                decay_step = 0
+                
+                for episode in range(total_episodes):
+                    # Set step to 0
+                    step = 0
+                    
+                    # Initialize the rewards of the episode
+                    episode_rewards = []
+                    
+                    # Make a new episode and observe the first state
+                    state = env.reset()
+                    
+                    # Remember that stack frame function also call our preprocess function.
+                    state, stacked_frames = stack_frames(stacked_frames, state, True)
+                    
+                    while step < max_steps:
+                        step += 1
+                        
+                        #Increase decay_step
+                        decay_step +=1
+                        
+                        # Predict the action to take and take it
+                        action, explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step, state, possible_actions)
+                        
+                        #Perform the action and get the next_state, reward, and done information
+                        next_state, reward, done, _ = env.step(action)
+                        
+                        if episode_render:
+                            env.render()
+                        
+                        # Add the reward to total reward
+                        episode_rewards.append(reward)
+                        
+                        # If the game is finished
+                        if done:
+                            # The episode ends so no next state
+                            next_state = np.zeros((110,84), dtype=np.int)
+                            
+                            next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+
+                            # Set step = max_steps to end the episode
+                            step = max_steps
+
+                            # Get the total reward of the episode
+                            total_reward = np.sum(episode_rewards)
+
+                            print('Episode: {}'.format(episode),
+                                        'Total reward: {}'.format(total_reward),
+                                        'Explore P: {:.4f}'.format(explore_probability),
+                                        'Training Loss {:.4f}'.format(loss))
+
+                            rewards_list.append((episode, total_reward))
+
+                            # Store transition <st,at,rt+1,st+1> in memory D
+                            memory.add((state, action, reward, next_state, done))
+
+                        else:
+                            # Stack the frame of the next_state
+                            next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+                        
+                            # Add experience to memory
+                            memory.add((state, action, reward, next_state, done))
+
+                            # st+1 is now our current state
+                            state = next_state
+                            
+
+                        ### LEARNING PART            
+                        # Obtain random mini-batch from memory
+                        batch = memory.sample(batch_size)
+                        states_mb = np.array([each[0] for each in batch], ndmin=3)
+                        actions_mb = np.array([each[1] for each in batch])
+                        rewards_mb = np.array([each[2] for each in batch]) 
+                        next_states_mb = np.array([each[3] for each in batch], ndmin=3)
+                        dones_mb = np.array([each[4] for each in batch])
+
+                        target_Qs_batch = []
+
+                        # Get Q values for next_state 
+                        Qs_next_state = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: next_states_mb})
+                        
+                        # Set Q_target = r if the episode ends at s+1, otherwise set Q_target = r + gamma*maxQ(s', a')
+                        for i in range(0, len(batch)):
+                            terminal = dones_mb[i]
+
+                            # If we are in a terminal state, only equals reward
+                            if terminal:
+                                target_Qs_batch.append(rewards_mb[i])
+                                
+                            else:
+                                target = rewards_mb[i] + gamma * np.max(Qs_next_state[i])
+                                target_Qs_batch.append(target)
+                                
+
+                        targets_mb = np.array([each for each in target_Qs_batch])
+
+                        loss, _ = sess.run([DQNetwork.loss, DQNetwork.optimizer],
+                                                feed_dict={DQNetwork.inputs_: states_mb,
+                                                        DQNetwork.target_Q: targets_mb,
+                                                        DQNetwork.actions_: actions_mb})
+
+                        # Write TF Summaries
+                        summary = sess.run(write_op, feed_dict={DQNetwork.inputs_: states_mb,
+                                                            DQNetwork.target_Q: targets_mb,
+                                                            DQNetwork.actions_: actions_mb})
+                        writer.add_summary(summary, episode)
+                        writer.flush()
+
+                    # Save model every 5 episodes
+                    if episode % 5 == 0:
+                        save_path = saver.save(sess, "./models/model.ckpt")
+                        print("Model Saved")
 
 
     def preprocess(self,frame):
@@ -78,12 +254,9 @@ class QLearningPlayer(Player):
         1. Need to gray scale the frame if it is not already 
         3. Need to flatten the frame down to a 1D array 
         '''
-        print("This is the type of the frame ",type(frame))
-        print(frame)
+        
         normalized = frame/255.0
-
         flattened_frame = normalized.flatten()
-        print(flattened_frame.shape)
         return flattened_frame
     
 
@@ -110,3 +283,39 @@ class QLearningPlayer(Player):
             #Build the stacked state 
             stacked_state = np.stack(self.stacked_frames,axis = 2 )
         return self.stacked_state, stacked_frames
+    
+
+
+
+
+
+
+
+
+
+
+def predict_action(explore_start, explore_stop, decay_rate, decay_step, state, actions):
+    ## EPSILON GREEDY STRATEGY
+    # Choose action a from state s using epsilon greedy.
+    ## First we randomize a number
+    exp_exp_tradeoff = np.random.rand()
+
+    # Here we'll use an improved version of our epsilon greedy strategy used in Q-learning notebook
+    explore_probability = explore_stop + (explore_start - explore_stop) * np.exp(-decay_rate * decay_step)
+    
+    if (explore_probability > exp_exp_tradeoff):
+        # Make a random action (exploration)
+        choice = random.randint(1,len(possible_actions))-1
+        action = possible_actions[choice]
+        
+    else:
+        # Get action from Q-network (exploitation)
+        # Estimate the Qs values state
+        Qs = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: state.reshape((1, *state.shape))})
+        
+        # Take the biggest Q value (= the best action)
+        choice = np.argmax(Qs)
+        action = possible_actions[choice]
+                
+                
+    return action, explore_probability
